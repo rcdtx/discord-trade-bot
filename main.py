f6faeb000f22e006e945a3ea9c2246c324ce1caa
rcdhtx <rcdhtx@gmail.com>
2023-02-05 18:35:21 -0500

    updates to buy and sell command logic

--- main.py
+++ main.py
@@ -17,30 +17,29 @@ bot = commands.Bot(command_prefix=">", intents=intents)
 
 @bot.event
 async def on_message(message):
+    if message.author == bot.user:
+        return
+
     user_id = message.author.id
+
     if message.content.startswith("!buy"):
         # Get current price of symbol from CoinGecko API
         symbol = message.content.split()[1]
-        coins_list = requests.get("https://api.coingecko.com/api/v3/coins/list").json()
-
-        for coin in coins_list:
-            if coin["symbol"] == symbol:
-                coin_id = coin["id"]
-                break
-            else:
-                coin_id = None
-
-        if coin_id:
-            price = requests.get(
-                f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
-            ).json()[coin_id]["usd"]
-        else:
-            print(f"Error: Symbol '{symbol}' not found in CoinGecko API")
-            price = None
+        amount = float(message.content.split()[2])
 
         if user_id in balances:
-            if balances[user_id] >= price:
-                balances[user_id] -= price
+            if balances[user_id]["usd"] >= amount:
+                price = get_price(symbol)
+
+                if price is None:
+                    await message.channel.send(
+                        f"{message.author.mention}, Unable to retrieve the price for {symbol}."
+                    )
+                    return
+
+                balances[user_id]["usd"] -= amount
+                balances[user_id][symbol] += amount / price
+
                 await message.channel.send(
                     f"{message.author.mention}, you have successfully bought {symbol} for ${price}. Your new balance is ${balances[user_id]}"
                 )
@@ -55,12 +54,19 @@ async def on_message(message):
 
     elif message.content.startswith("!sell"):
         symbol = message.content.split()[1]
+        amount = float(message.content.split()[2])
+
         # Get current price of symbol from CoinGecko API
-        price = requests.get(
-            f"https://api.coingecko.com/api/v3/simple/price?ids={symbol}&vs_currencies=usd"
-        ).json()[symbol]["usd"]
+        price = get_price(symbol)
+
         if user_id in balances:
-            balances[user_id] += price
+            if amount > 0 and amount <= balances[user_id][symbol]:
+                balances[user_id]["usd"] += price * amount
+            else:
+                await message.channel.send(
+                    f"{message.author.mention}, you do not have enough {symbol} in your account to sell {amount}."
+                )
+
             await message.channel.send(
                 f"{message.author.mention}, you have successfully sold {symbol} for ${price}. Your new balance is ${balances[user_id]}"
             )
@@ -75,13 +81,36 @@ async def on_message(message):
                 f"{message.author.mention}, your balance is ${balances[user_id]}"
             )
         else:
-            balances[user_id] = 1000
+            balances[user_id]["usd"] = 1000.0
             await message.channel.send(
                 f"{message.author.mention}, an account has been set up for you with a balance of ${balances[user_id]}"
             )
 
     elif message.content.startswith("!leaderboard"):
         await message.channel.send(await sort_and_convert_leaderboard())
+
+
+async def get_price(symbol: str) -> float:
+    coins_list = await requests.get(
+        "https://api.coingecko.com/api/v3/coins/list"
+    ).json()
+
+    for coin in coins_list:
+        if coin["symbol"] == symbol:
+            coin_id = coin["id"]
+            break
+        else:
+            coin_id = None
+
+    if coin_id:
+        price = await requests.get(
+            f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
+        ).json()[coin_id]["usd"]
+    else:
+        print(f"Error: Symbol '{symbol}' not found in CoinGecko API")
+        price = None
+
+    return float(price)
 
 
 async def sort_and_convert_leaderboard() -> dict:
